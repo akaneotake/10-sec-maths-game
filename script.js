6b468bbcb126ce2d28e85549f524de01dc93e5d2
akaneotake <akane1033@gmail.com>
2023-11-30 16:47:51 +0100

    'countdownTimer' function

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
 
+  // Countdown timer
+  var time = 10;
+  var countdownTimer = function() {
+    if (time > 0) {
+      console.log(time);
+      time--;
+      setTimeout(countdownTimer, 1000);
+    } else {
+      console.log("--END--");
+    }
+  };
+
+  // Generate Questions
   var generateQuestions = function() {
+    // 0~9
     var num1 = Math.floor(Math.random() * 10);
     var num2 = Math.floor(Math.random() * 10);
     var answer = num1 + num2;
-    console.log(num1 + "+" + num2 + "=" + answer);
     $('#equation').append(num1 + "+" + num2 + "=" + answer);
   };
 
+  countdownTimer();
   generateQuestions();
 
 });
