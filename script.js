5817a9898a2a6136bd5dd1adc8c48bd6ed7fce90
akaneotake <akane1033@gmail.com>
2023-12-01 11:40:48 +0100

    'checkAnswer' function

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
 
-  // Countdown timer
+  // [function] Countdown timer
   var time = 10;
+  $('#count').append(time);
+
   var countdownTimer = function() {
-    if (time >= 0) {
+    if (time > 0) {
       $('#count').empty();
-      $('#count').append(time);
       time--;
+      console.log(time);
       setTimeout(countdownTimer, 1000);
+      $('#count').append(time);
     };
   };
 
-  // Generate Questions
-  var generateQuestions = function() {
+  // [function] Generate Question
+  var answer = 0;
+  var generateQuestion = function() {
     // 0~9
     var num1 = Math.floor(Math.random() * 10);
     var num2 = Math.floor(Math.random() * 10);
-    var answer = num1 + num2;
-    $('#equation').append(num1 + "+" + num2 + "=" + answer);
+    answer = num1 + num2;
+    $('#equation').append(num1 + "+" + num2);
   };
 
-  countdownTimer();
-  generateQuestions();
+  // [function] Check Answer
+  var checkAnswer = function() {
+    if ($('#answer').val() == answer) {
+      time++;
+      console.log("correct");
+      $('#equation').text('');
+      $('#answer').val('');
+      generateQuestion();
+    };
+  };
 
+  // Events
+  $('#answer').on('click', countdownTimer);
+  $('#answer').on('input', checkAnswer);
+  generateQuestion();
 });
